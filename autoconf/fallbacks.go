2817d120672d77af4136aad23a3d4f1ee377cf4c
nnlgsakib <nlgarts@gmail.com>
2025-09-20 19:11:33 +0600

    added new auto conf

--- autoconf/fallbacks.go
+++ autoconf/fallbacks.go
@@ -5,15 +5,7 @@ package autoconf
 var (
 	// FallbackBootstrapPeers are the default bootstrap peers from Kubo 0.36
 	// Used as last-resort fallback when autoconf fetch fails
-	FallbackBootstrapPeers = []string{
-		"/dnsaddr/bootstrap.libp2p.io/p2p/QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN",
-		"/dnsaddr/bootstrap.libp2p.io/p2p/QmQCU2EcMqAqQPR2i9bChDtGNJchTbq5TbXJJ16u19uLTa",
-		"/dnsaddr/bootstrap.libp2p.io/p2p/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb",
-		"/dnsaddr/bootstrap.libp2p.io/p2p/QmcZf59bWwK5XFi76CZX8cbJ4BhTzzA3gU1ZjYZcYW3dwt",
-		"/dnsaddr/va1.bootstrap.libp2p.io/p2p/12D3KooWKnDdG3iXw9eTFijk3EWSunZcFi54Zka4wmtqtt6rPxc8",
-		"/ip4/104.131.131.82/tcp/4001/p2p/QmaCpDMGvV2BGHeYERUEnRQAwe3N8SzbUtfsmvsqQLuvuJ",
-		"/ip4/104.131.131.82/udp/4001/quic-v1/p2p/QmaCpDMGvV2BGHeYERUEnRQAwe3N8SzbUtfsmvsqQLuvuJ",
-	}
+	FallbackBootstrapPeers = []string{}
 
 	// FallbackDNSResolvers are the default DNS resolvers matching mainnet autoconf
 	// Used as last-resort fallback when autoconf fetch fails
@@ -36,8 +28,8 @@ func GetMainnetFallbackConfig() *Config {
 		AutoConfSchema:  1,
 		SystemRegistry: map[string]SystemConfig{
 			SystemAminoDHT: {
-				URL:         "https://github.com/ipfs/specs/pull/497",
-				Description: "Public DHT swarm that implements the IPFS Kademlia DHT specification under protocol identifier /ipfs/kad/1.0.0",
+				URL:         "",
+				Description: "Public DHT swarm that implements the IPFS Kademlia DHT specification under protocol identifier /wwfs/kad/1.0.0",
 				NativeConfig: &NativeConfig{
 					Bootstrap: FallbackBootstrapPeers,
 				},
